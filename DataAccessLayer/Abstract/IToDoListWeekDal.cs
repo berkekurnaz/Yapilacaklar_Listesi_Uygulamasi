bf2749ceeb9e0b47623d9731c8f11b185a78a0bb
BerkeKurnaz <bkoyunberkekurnaz@hotmail.com>
2019-06-25 16:05:17 +0300

    Created Data Access Layer ToDoListWeek

---
+++ DataAccessLayer/Abstract/IToDoListWeekDal.cs
@@ -0,0 +1,18 @@
+﻿using KisiselYapilacaklarApp.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KisiselYapilacaklarApp.DataAccessLayer.Abstract
+{
+    public interface IToDoListWeekDal
+    {
+        List<ToDoListWeek> GetAll();
+        ToDoListWeek GetById(int Id);
+        void Add(ToDoListWeek toDoListWeek);
+        void Update(int Id, ToDoListWeek toDoListWeek);
+        void Delete(int Id);
+    }
+}
---
+++ DataAccessLayer/Concrete/ToDoListWeekDal.cs
@@ -0,0 +1,97 @@
+﻿using KisiselYapilacaklarApp.DataAccessLayer.Abstract;
+using KisiselYapilacaklarApp.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KisiselYapilacaklarApp.DataAccessLayer.Concrete
+{
+    public class ToDoListWeekDal : IToDoListWeekDal
+    {
+
+        AppSQLiteConnection connection = new AppSQLiteConnection();
+
+        public List<ToDoListWeek> GetAll()
+        {
+            List<ToDoListWeek> todos = new List<ToDoListWeek>();
+            string query = "Select * From ToDoListWeek";
+            SQLiteCommand command = new SQLiteCommand(query, connection.Connection());
+            SQLiteDataReader dr = command.ExecuteReader();
+            while (dr.Read())
+            {
+                ToDoListWeek todo = new ToDoListWeek();
+                todo.Id = Convert.ToInt16(dr["Id"]);
+                todo.Title = dr["Title"].ToString();
+                todo.Week = Convert.ToInt16(dr["Week"]);
+                todo.Month = Convert.ToInt16(dr["Month"]);
+                todo.Year = Convert.ToInt16(dr["Year"]);
+                todo.Completed = dr["Completed"].ToString();
+                todos.Add(todo);
+            }
+            dr.Close();
+            connection.Connection().Close();
+            return todos;
+        }
+
+        public ToDoListWeek GetById(int Id)
+        {
+            ToDoListWeek todo = new ToDoListWeek();
+            string query = "Select * From ToDoListWeek Where Id=@p1";
+            SQLiteCommand command = new SQLiteCommand(query, connection.Connection());
+            command.Parameters.AddWithValue("@p1", Id);
+            SQLiteDataReader dr = command.ExecuteReader();
+            while (dr.Read())
+            {
+                todo.Id = Convert.ToInt16(dr["Id"]);
+                todo.Title = dr["Title"].ToString();
+                todo.Week = Convert.ToInt16(dr["Week"]);
+                todo.Month = Convert.ToInt16(dr["Month"]);
+                todo.Year = Convert.ToInt16(dr["Year"]);
+                todo.Completed = dr["Completed"].ToString();
+            }
+            dr.Close();
+            connection.Connection().Close();
+            return todo;
+        }
+
+        public void Add(ToDoListWeek toDoListWeek)
+        {
+            string query = "Insert Into ToDoListWeek (Title,Week,Month,Year,Completed) Values (@p1,@p2,@p3,@p4,@p5)";
+            SQLiteCommand command = new SQLiteCommand(query, connection.Connection());
+            command.Parameters.AddWithValue("@p1", toDoListWeek.Title);
+            command.Parameters.AddWithValue("@p2", toDoListWeek.Week);
+            command.Parameters.AddWithValue("@p3", toDoListWeek.Month);
+            command.Parameters.AddWithValue("@p4", toDoListWeek.Year);
+            command.Parameters.AddWithValue("@p5", toDoListWeek.Completed);
+            command.ExecuteNonQuery();
+            connection.Connection().Close();
+        }
+
+        public void Update(int Id, ToDoListWeek toDoListWeek)
+        {
+            string query = "Update ToDoListWeek set Title=@p1, Week=@p2, Month=@p3, Year=@p4, Completed=@p5 where Id=@p6";
+            SQLiteCommand command = new SQLiteCommand(query, connection.Connection());
+            command.Parameters.AddWithValue("@p1", toDoListWeek.Title);
+            command.Parameters.AddWithValue("@p2", toDoListWeek.Week);
+            command.Parameters.AddWithValue("@p3", toDoListWeek.Month);
+            command.Parameters.AddWithValue("@p4", toDoListWeek.Year);
+            command.Parameters.AddWithValue("@p5", toDoListWeek.Completed);
+            command.Parameters.AddWithValue("@p6", toDoListWeek.Id);
+            command.ExecuteNonQuery();
+            connection.Connection().Close();
+        }
+
+        public void Delete(int Id)
+        {
+            string query = "Delete From ToDoListWeek Where Id=@p1";
+            SQLiteCommand command = new SQLiteCommand(query, connection.Connection());
+            command.Parameters.AddWithValue("@p1", Id);
+            command.ExecuteNonQuery();
+            connection.Connection().Close();
+        }
+
+    }
+}
