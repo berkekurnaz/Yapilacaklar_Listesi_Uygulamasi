300924175cb2684674cb085d129e1d01331b1f6b
BerkeKurnaz <bkoyunberkekurnaz@hotmail.com>
2019-06-26 15:59:49 +0300

    Created Business Layer ToDoListWeekManager

---
+++ BusinessLayer/Abstract/IToDoListWeekManager.cs
@@ -0,0 +1,18 @@
+﻿using KisiselYapilacaklarApp.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KisiselYapilacaklarApp.BusinessLayer.Abstract
+{
+    public interface IToDoListWeekManager
+    {
+        List<ToDoListWeek> GetAll();
+        ToDoListWeek GetById(int Id);
+        void Add(ToDoListWeek toDoListWeek);
+        void Update(int Id, ToDoListWeek toDoListWeek);
+        void Delete(int Id);
+    }
+}
---
+++ BusinessLayer/Concrete/ToDoListWeekManager.cs
@@ -0,0 +1,75 @@
+﻿using KisiselYapilacaklarApp.BusinessLayer.Abstract;
+using KisiselYapilacaklarApp.DataAccessLayer.Concrete;
+using KisiselYapilacaklarApp.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KisiselYapilacaklarApp.BusinessLayer.Concrete
+{
+    public class ToDoListWeekManager : IToDoListWeekManager
+    {
+
+        ToDoListWeekDal toDoListWeekDal = new ToDoListWeekDal();
+
+        public List<ToDoListWeek> GetAll()
+        {
+            return toDoListWeekDal.GetAll();
+        }
+
+        public ToDoListWeek GetById(int Id)
+        {
+            var todo = toDoListWeekDal.GetById(Id);
+            if (todo == null)
+            {
+                throw new Exception("Error");
+            }
+            return todo;
+        }
+
+        public void Add(ToDoListWeek toDoListWeek)
+        {
+            try
+            {
+                if (toDoListWeek.Title.Length > 2)
+                {
+                    toDoListWeekDal.Add(toDoListWeek);
+                }
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+        }
+
+        public void Update(int Id, ToDoListWeek toDoListWeek)
+        {
+            try
+            {
+                if (toDoListWeek.Title.Length > 2)
+                {
+                    toDoListWeekDal.Update(Id, toDoListWeek);
+                }
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+        }
+
+        public void Delete(int Id)
+        {
+            try
+            {
+                toDoListWeekDal.Delete(Id);
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+        }
+
+    }
+}
